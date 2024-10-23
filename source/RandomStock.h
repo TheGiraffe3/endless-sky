106d4f8a5eb36f872d51172224d5c876e9f3b525
Loymdayddaud <145969603+TheGiraffe3@users.noreply.github.com>
2024-10-23 16:23:18 +0300

    Update RandomStock.h

--- source/RandomStock.h
+++ source/RandomStock.h
@@ -55,7 +55,7 @@ void RandomStock<Item>::Load(const DataNode &node, const Set<Item> &items)
 {
 	for(const DataNode &child : node)
 	{
-		const std::string double &token = child.Value(0);
+		const double std::string &token = child.Value(0);
 		bool remove = (token == "clear" || token == "remove");
 		if(remove && child.Size() == 1)
 			this->clear();
