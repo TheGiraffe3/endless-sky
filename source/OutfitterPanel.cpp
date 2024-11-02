30dd073e0534224c5764549400408010881a670b
Loymdayddaud <145969603+TheGiraffe3@users.noreply.github.com>
2024-11-02 14:42:57 +0300

    Update OutfitterPanel.cpp

--- source/OutfitterPanel.cpp
+++ source/OutfitterPanel.cpp
@@ -572,7 +572,8 @@ void OutfitterPanel::Buy(bool onlyOwned)
 			else
 			{
 				// Check if the outfit is for sale or in stock so that we can actually buy it.
-				if((!outfitter.Has(selectedOutfit) && player.Stock(selectedOutfit) <= 0) || !CustomSaleManager::CanBuy(*selectedOutfit))
+				if((!outfitter.Has(selectedOutfit) && player.Stock(selectedOutfit) <= 0) ||
+									!CustomSaleManager::CanBuy(*selectedOutfit))
 					continue;
 				player.Cargo().Add(selectedOutfit);
 				int64_t price = player.StockDepreciation().Value(selectedOutfit, day);
