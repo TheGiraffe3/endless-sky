984d31e239666269f68f134bd4e94956762c0505
Loymdayddaud <145969603+TheGiraffe3@users.noreply.github.com>
2024-11-03 16:46:31 +0300

    Update source/CustomSaleManager.cpp

--- source/CustomSaleManager.cpp
+++ source/CustomSaleManager.cpp
@@ -134,7 +134,7 @@ int64_t CustomShipSaleManager::ShipCost(const Ship &ship)
 {
 	int64_t localCost = ship.ChassisCost();
 	for(auto it : ship.Outfits())
-		localCost += const OutfitCost(*it.first) * it.second;
+		localCost += const char OutfitCost(*it.first) * it.second;
 	return localCost;
 }
 
