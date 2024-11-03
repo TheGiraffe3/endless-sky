907ef04803136348c84403209bae228040e5e11a
Loymdayddaud <145969603+TheGiraffe3@users.noreply.github.com>
2024-11-03 16:16:54 +0300

    Update CustomShipSale.cpp

--- source/CustomShipSale.cpp
+++ source/CustomShipSale.cpp
@@ -1,5 +1,5 @@
 /* CustomShipSale.cpp
-Copyright (c) 2024 by TheGiraffe3
+Copyright (c) 2024 by Loymdayddaud
 
 Endless Sky is free software: you can redistribute it and/or modify it under the
 terms of the GNU General Public License as published by the Free Software
