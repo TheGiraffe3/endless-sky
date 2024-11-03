9b25cc582601563a384f94fbcb2ae2fc1f855a28
Loymdayddaud <145969603+TheGiraffe3@users.noreply.github.com>
2024-11-03 16:16:27 +0300

    Update CustomShipSale.h

--- source/CustomShipSale.h
+++ source/CustomShipSale.h
@@ -1,5 +1,5 @@
 /* CustomShipSale.h
-Copyright (c) 2024 by Hurleveur
+Copyright (c) 2024 by Loymdayddaud
 
 Endless Sky is free software: you can redistribute it and/or modify it under the
 terms of the GNU General Public License as published by the Free Software
