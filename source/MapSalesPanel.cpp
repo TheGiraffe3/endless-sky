11c49c6186cb792107d83ea5aeb14d02856115bf
Loymdayddaud <145969603+TheGiraffe3@users.noreply.github.com>
2024-11-04 11:04:56 +0300

    Update source/MapSalesPanel.cpp

--- source/MapSalesPanel.cpp
+++ source/MapSalesPanel.cpp
@@ -175,7 +175,7 @@ bool MapSalesPanel::Click(int x, int y, int clicks)
 	{
 		const System *previousSystem = MapPanel::selectedSystem;
 		bool result = MapPanel::Click(x, y, clicks);
-		if(MapPanel::selectedSystem != previousSystem)
+	if(MapPanel::selectedSystem != previousSystem)
 			CustomOutfitSaleManager::Refresh(MapPanel::selectedSystem, player.Conditions());
 			CustomShipSaleManager::Refresh(MapPanel::selectedSystem, player.Conditions());
 		return result;
