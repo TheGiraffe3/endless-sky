fb26f583f27c1c8325415e7ce98095c829795e09
Loymdayddaud <145969603+TheGiraffe3@users.noreply.github.com>
2024-11-06 19:11:17 +0300

    Update Depreciation.h

--- source/Depreciation.h
+++ source/Depreciation.h
@@ -86,4 +86,9 @@ private:
 
 	std::map<const Ship *, std::map<int, int>> ships;
 	std::map<const Outfit *, std::map<int, int>> outfits;
+
+
+private:
+	// Calculate the value fraction for an item of the given age.
+	static double Depreciate(int age);
 };
