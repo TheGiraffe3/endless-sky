a6847c3b3127d38c9cfc8c2c4eda0728a3875c50
Loymdayddaud <145969603+TheGiraffe3@users.noreply.github.com>
2024-11-09 09:50:16 +0300

    Apply suggestions from code review

--- source/PlayerInfo.cpp
+++ source/PlayerInfo.cpp
@@ -4087,8 +4087,7 @@ void PlayerInfo::CreateRandomStock()
 	int day = GetDate().DaysSinceEpoch();
 	for(const auto &rStock : planet->OutfitRandomStock())
 		for(const auto &stockItem : *rStock)
-			if "to stock" = ConditionSet
-			{
+			if rStock = ConditionSet {
 				outfitStock[stockItem.item] += stockItem.quantity;
 				for(unsigned int i = 0; i < stockItem.quantity; i++)
 					stockDepreciation.Buy(stockItem.item, day - stockItem.depreciation, nullptr);
